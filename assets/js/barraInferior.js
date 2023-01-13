dfbc011482aef1915b254040e4f5f5a06d80a1b1
Welington <108595989+Welingtonlramos@users.noreply.github.com>
2023-01-13 20:54:17 -0300

    Melhorias visuais

--- assets/js/barraInferior.js
+++ assets/js/barraInferior.js
@@ -3,15 +3,28 @@ const voltaTopo = document.querySelector("#botao-topo");
 const pesquisa = document.querySelector("#botao-pesquisa");
 const notificacao = document.querySelector("#botao-confeccao");
 
+const barraDePesquisa = document.querySelector("#barra-pesquisa");
+const background = document.querySelector('.pesquisa-background');
+
+
 //      Volta ao topo
 voltaTopo.addEventListener("click", (evento) => {
     fechaMenu();
     window.scrollTo(0,0);
 });
 
+
+// Pesquisa modelos
 pesquisa.addEventListener("click", (evento) => {
     fechaMenu();
+    if (barraDePesquisa.classList.contains('some')) {
+        barraDePesquisa.classList.remove("some");
+    } else {barraDePesquisa.classList.add('some');}
 });
+
+background.addEventListener("click", (evento) => {
+    barraDePesquisa.classList.add('some');
+})
 
 notificacao.addEventListener("click", (evento) => {
     fechaMenu();
